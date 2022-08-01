93843a940fe738f8a4f2520a1fcf41bd3d5691f5
Brent Andries <brent.andries2000@gmail.com>
2022-08-01 18:27:35 +0200

    CSS consistency and rel="noreferrer"
    Changed _ to - for css naming consistency
    
    Added rel="noreferrer" to href with target: _blank

--- src/Components/PokemonUICard.js
+++ src/Components/PokemonUICard.js
@@ -14,17 +14,17 @@ export default function PokemonUICard(props) {
     
     return (
         <section>
-            <a href={`https://www.pokemon.com/nl/pokedex/${pokemon.name}`} target="_blank">
+            <a href={`https://www.pokemon.com/nl/pokedex/${pokemon.name}`} target="_blank" rel="noreferrer">
                 <div className="grid">
-                    <h3 className="grid_item">Pokedex ID:</h3>
-                    <h3 className="grid_item">name:</h3> 
-                    <h3 className="grid_item">type(s):</h3> 
-                    {pokemon.stats?.map(stat => <h3 className="grid_item">{stat.stat.name}:</h3>)}
+                    <h3 className="grid-item">Pokedex ID:</h3>
+                    <h3 className="grid-item">name:</h3> 
+                    <h3 className="grid-item">type(s):</h3> 
+                    {pokemon.stats?.map(stat => <h3 className="grid-item">{stat.stat.name}:</h3>)}
 
-                    <p className="grid_item">#{pokemon.id}</p>
-                    <p className="grid_item">{pokemon.name}</p>
-                    <p className="grid_item">{pokemon.types?.map(type => <>{type.type.name} </> )}</p>
-                    {pokemon.stats?.map(stat => <p className="grid_item">{stat.base_stat} </p>)}
+                    <p className="grid-item">#{pokemon.id}</p>
+                    <p className="grid-item">{pokemon.name}</p>
+                    <p className="grid-item">{pokemon.types?.map(type => <>{type.type.name} </> )}</p>
+                    {pokemon.stats?.map(stat => <p className="grid-item">{stat.base_stat} </p>)}
                 </div>
             </a>
         </section>
